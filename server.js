ff6ee6cdb6f59d4e08e7b826604ab8d77c8356c2
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-09-27 15:57:33 -0700

    .env

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
 
 
 const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
